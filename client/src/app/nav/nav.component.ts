6c6b92340ad94e21fa117175d0d001e25512e411
Cleiton <cleiton.siqueira.266@gmail.com>
2024-11-07 19:41:35 -0300

    Added messages, members and member details components, routes updated

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,27 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
     selector: 'app-nav',
     standalone: true,
-    imports: [FormsModule, TitleCasePipe, BsDropdownModule],
+    imports: [FormsModule, TitleCasePipe, BsDropdownModule, RouterLink, RouterLinkActive],
     templateUrl: './nav.component.html',
     styleUrl: './nav.component.css'
 })
 
 export class NavComponent {
     accountService = inject(AccountService);
+    private router = inject(Router);
 
     @Input() title: any;
     model: any = {};
 
     login() {
         this.accountService.login(this.model).subscribe({
-            next: response => {
-                console.log(response);
-                this.model = response;
+            next: () => {
+                this.router.navigateByUrl('/members');
             },
             error: error => console.log(error)
         })
@@ -31,6 +32,7 @@ export class NavComponent {
 
     logout() {
         this.accountService.logout();
+        this.router.navigateByUrl('/');
     }
 
 }
