a57d1430479be3c07e6bc1a96811b3637f3f961a
Cleiton <cleiton.siqueira.266@gmail.com>
2024-11-12 16:10:36 -0300

    Added JWT interceptor

--- client/src/app/errors/not-found/not-found.component.ts
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+    selector: 'app-not-found',
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: './not-found.component.html',
+    styleUrl: './not-found.component.css',
+    encapsulation: ViewEncapsulation.None
 })
 export class NotFoundComponent {
 
