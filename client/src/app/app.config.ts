64d52b54d45077b5b23542e416ed1dcc3712ac86
Cleiton <cleiton.siqueira.266@gmail.com>
2024-11-13 23:16:30 -0300

    Updated edit form with update function

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from "@angular/platform-browser/animations";
 
@@ -7,6 +7,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,6 +19,7 @@ export const appConfig: ApplicationConfig = {
             progressBar: true,
             tapToDismiss: false,
             closeButton: true
-        })
+        }),
+        importProvidersFrom(NgxSpinnerModule)
     ]
 };
