c92e9f8542c1a0ed314bd34d9fe66f4805a2f824
Cleiton <cleiton.siqueira.266@gmail.com>
2024-11-25 19:47:07 -0300

    Code refactored to accommodate Identity Framework

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -25,8 +25,6 @@ namespace API.Controllers
 
             var user = mapper.Map<AppUser>(registerDTO);
             user.UserName = registerDTO.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password));
-            user.PasswordSalt = hmac.Key;
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
@@ -48,14 +46,6 @@ namespace API.Controllers
             .FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if(user == null) return Unauthorized("Invalid username.");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password.");
-            }
 
             return new UserDto
             {
