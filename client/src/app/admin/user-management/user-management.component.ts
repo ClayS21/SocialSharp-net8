15cf7f9ae2d56255ef709cd4707a06549b64b6e9
Cleiton <cleiton.siqueira.266@gmail.com>
2024-11-27 15:56:23 -0300

    Added edit modal

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminService } from '../../_services/admin.service';
+import { User } from '../../_models/user';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.component';
+import { initialState } from 'ngx-bootstrap/timepicker/reducer/timepicker.reducer';
 
 @Component({
   selector: 'app-user-management',
@@ -7,6 +12,30 @@ import { Component } from '@angular/core';
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
+    private adminService = inject(AdminService);
+    private modalService = inject(BsModalService);
+    users: User[] = [];
+    bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
+    ngOnInit(): void {
+        this.getUsersWithRoles();
+    }
+
+    openRolesModal() {
+        const initialState: ModalOptions = {
+            class: 'modal-lg',
+            initialState: {
+                title: 'User roles',
+                lists: ['Admin', 'Moderator', 'Member']
+            }
+        }
+        this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    }
+
+    getUsersWithRoles() {
+        this.adminService.getUserWithRoles().subscribe({
+            next: users => this.users = users
+        })
+    }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -10,6 +10,7 @@ import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './_interceptors/loading.interceptor';
 import { TimeagoModule } from 'ngx-timeago';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,10 +19,8 @@ export const appConfig: ApplicationConfig = {
         provideAnimations(),
         provideToastr({
             positionClass: 'toast-bottom-right',
-            progressBar: true,
-            tapToDismiss: false,
-            closeButton: true
+            progressBar: true
         }),
-        importProvidersFrom(NgxSpinnerModule, TimeagoModule.forRoot())
+        importProvidersFrom(NgxSpinnerModule, TimeagoModule.forRoot(), ModalModule.forRoot())
     ]
 };
